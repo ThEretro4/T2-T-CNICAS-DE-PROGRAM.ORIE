f560def0525c7b43c3db8710f3147cca7259daae
GANCITOS <losadaangel123@gmail.com>
2026-05-13 18:58:49 -0500

    Update T22.java

--- T22.java
+++ T22.java
@@ -7,7 +7,7 @@ public class T22 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola Git");
     }
     
 }
